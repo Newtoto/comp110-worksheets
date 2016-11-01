e7c394102a9acac18575ba9b1c56970ad298926f
Andrew Findlay <andrewrfindlay@ntlworld.com>
2016-11-01 08:29:45 +0000

    Added some initial ideas to each segment

--- worksheet_D/oxo.py
+++ worksheet_D/oxo.py
@@ -1,26 +1,65 @@
 class OxoBoard:
     def __init__(self):
         """ The initialiser. Initialise any fields you need here. """
-        raise NotImplementedError("TODO: implement __init__")
+        boardList = [[0, 0, 0],
+                 [0, 0, 0],
+                 [0, 0, 0]]
+
+        # raise NotImplementedError("TODO: implement __init__")
 
     def get_square(self, x, y):
         """ Return 0, 1 or 2 depending on the contents of the specified square. """
-        raise NotImplementedError("TODO: implement get_square")
+        self.x = x
+        self.y = y
+
+        return boardList[self.y * 3 + self.x]
+        # raise NotImplementedError("TODO: implement get_square")
 
     def set_square(self, x, y, mark):
         """ If the specified square is currently empty (0), fill it with mark and return True.
             If the square is not empty, leave it as-is and return False. """
-        raise NotImplementedError("TODO: implement set_square")
+        if boardList[self.y * 3 + self.x] == 0:
+            boardList[self. y * 3 + self.x] = mark
+        else:
+            return False
+        # raise NotImplementedError("TODO: implement set_square")
 
     def is_board_full(self):
         """ If there are still empty squares on the board, return False.
             If there are no empty squares, return True. """
-        raise NotImplementedError("TODO: implement is_board_full")
+        # cycles through x, 0, 1, 2 and for each does y, 0, 1, 2. If any are empty false is returned and the cycle breaks. If the cycle finishes it returns true.
+        for y in xrange(3):
+            for x in xrange(3):
+                if boardList[y * 3 + x] == 0:
+                    return False
+                    break
+            return True
+
+        # raise NotImplementedError("TODO: implement is_board_full")
 
     def get_winner(self):
         """ If a player has three in a row, return 1 or 2 depending on which player.
             Otherwise, return 0. """
-        raise NotImplementedError("TODO: implement get_winner")
+
+        if boardList[1] == boardList [2] == boardList[3]:  # horizontal wins
+            return boardList[1]
+        elif boardList[4] == boardList [5] == boardList[6]:
+            return boardList[4]
+        elif boardList[7] == boardList[8] == boardList[9]:
+            return boardList[7]
+        elif boardList[1] == boardList[4] == boardList[7]:  #vertical wins
+            return boardList[1]
+        elif boardList[2] == boardList[5] == boardList[8]:
+            return boardList[2]
+        elif boardList[3] == boardList[6] == boardList[9]:
+            return boardList[3]
+        elif boardList[1] == boardList[5] == boardList[9]:  #diagonal wins
+            return boardList[1]
+        elif boardList[3] == boardList[5] == boardList[7]:
+            return boardList[3]
+        else:
+            return 0
+        # raise NotImplementedError("TODO: implement get_winner")
 
     def show(self):
         """ Display the current board state in the terminal. You should not need to edit this. """
